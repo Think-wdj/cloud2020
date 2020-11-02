cd049f29111b4cec00d6ceea8663bf53b1b9faeb
“Eraser <wudongjun31@qq.com>
2020-11-03 07:19:11 +0800

    MyLoadBalancer

--- cloud-consumer-order80/src/main/java/com/eraser/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/eraser/springcloud/controller/OrderController.java
@@ -2,7 +2,10 @@ package com.eraser.springcloud.controller;
 
 import com.eraser.springcloud.entities.CommonResult;
 import com.eraser.springcloud.entities.Payment;
+import com.eraser.springcloud.lb.LoadBalancer;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.net.URI;
+import java.util.List;
 
 /**
  * @author wudongjun E-mail:wudongjun43@163.com
@@ -26,6 +31,12 @@ public class OrderController {
 
     @Resource
     private RestTemplate restTemplate;
+
+    @Resource
+    private LoadBalancer loadBalancer;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @GetMapping("/payment/create")
     public CommonResult<Payment> create(Payment payment){
@@ -46,5 +57,16 @@ public class OrderController {
         }else{
             return  new CommonResult<Payment>(406,"操作失败");
         }
+    }
+
+    @GetMapping(value = "/payment/lb")
+    public String getPaymentLB(){
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        if (instances==null||instances.size()<=0){
+            return null;
+        }
+        final ServiceInstance serviceInstance = loadBalancer.instances(instances);
+        final URI uri = serviceInstance.getUri();
+        return  this.restTemplate.getForObject(uri+"/payment/lb",String.class);
     }
 }
--- cloud-consumer-order80/src/main/java/com/eraser/springcloud/lb/MyLB.java
+++ cloud-consumer-order80/src/main/java/com/eraser/springcloud/lb/MyLB.java
@@ -22,13 +22,14 @@ public class MyLB implements LoadBalancer {
             current = this.atomicInteger.get();
             next = current >= Integer.MAX_VALUE ? 0 : current+1 ;
         }while (!this.atomicInteger.compareAndSet(current,next));
-        System.out.println("********next"+next);
+        System.out.println("********第几次访问，次数next："+next);
         return next;
     }
 
     @Override
     public ServiceInstance instances(List<ServiceInstance> serviceInstances) {
         int index = getAndIncrement() % serviceInstances.size();
-        return null;
+
+        return serviceInstances.get(index);
     }
 }
